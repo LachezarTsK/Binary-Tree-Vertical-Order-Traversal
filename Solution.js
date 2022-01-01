c23186fd4b4a1cb4cae529f5ead0286a4f46d31c
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-01-01 10:02:51 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,73 @@
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
+}
+
+//Pair is applied as a Linked List Node.
+function Pair(treeNode = null, vertical_level = - 1) {
+    this.treeNode = treeNode;
+    this.vertical_level = vertical_level;
+    this.next = null;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var verticalOrder = function (root) {
+
+    if (root === null) {
+        return [];
+    }
+    const MIN_TREE_NODE_VALUE = -100;
+    const MAX_TREE_NODE_VALUE = 100;
+
+    let head_queuePairs = new Pair(root, 0);
+    let tail_queuePairs = head_queuePairs;
+    const map_vertical_levels = new Map();
+    map_vertical_levels.set(0, [root.val]);
+
+    let minVertical = MAX_TREE_NODE_VALUE;
+    let maxVertical = MIN_TREE_NODE_VALUE;
+
+    while (head_queuePairs !== null) {
+
+        let pair = head_queuePairs;
+        minVertical = Math.min(minVertical, pair.vertical_level);
+        maxVertical = Math.max(maxVertical, pair.vertical_level);
+
+        if (pair.treeNode.left !== null) {
+
+            let vertical = pair.vertical_level - 1;
+            if (!map_vertical_levels.has(vertical)) {
+                map_vertical_levels.set(vertical, []);
+            }
+
+            map_vertical_levels.get(vertical).push(pair.treeNode.left.val);
+            tail_queuePairs.next = new Pair(pair.treeNode.left, vertical);
+            tail_queuePairs = tail_queuePairs.next;
+        }
+
+        if (pair.treeNode.right !== null) {
+
+            let vertical = pair.vertical_level + 1;
+            if (!map_vertical_levels.has(vertical)) {
+                map_vertical_levels.set(vertical, []);
+            }
+
+            map_vertical_levels.get(vertical).push(pair.treeNode.right.val);
+            tail_queuePairs.next = new Pair(pair.treeNode.right, vertical);
+            tail_queuePairs = tail_queuePairs.next;
+        }
+
+        head_queuePairs = head_queuePairs.next;
+    }
+
+    const nodesInVerticalOrder = [];
+    for (let i = minVertical; i <= maxVertical; i++) {
+        nodesInVerticalOrder.push(map_vertical_levels.get(i));
+    }
+    return nodesInVerticalOrder;
+};
